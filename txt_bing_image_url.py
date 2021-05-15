73f42e1f3eb8826c1be1eb6e8383f6c2c27fbe25
dagimal <daffagifariakmal@gmail.com>
2021-05-16 01:10:16 +0700

    bing

---
+++ txt_bing_image_url.py
@@ -0,0 +1,28 @@
+"""
+===+++ TXT Bing Image Url +++===
+Author : Dagimal
+Date : 15-May-2021
+E-mail : daffagifariakmal@gmail.com
+"""
+
+from bing_image_urls import bing_image_urls
+
+imgs_file = 'image_keyword.txt'
+imgs = open(imgs_file, 'r').readlines()
+print(imgs[1])
+print('FILE '+imgs_file+' ADA '+str(len(imgs))+' BARIS')
+print('INDEX AWAL NYA ADALAH 0')
+print('=========================\n')
+# INPUT
+fileName = input('Nama File Output : ')
+awal = int(input('Index Awal : '))
+akhir = int(input('Index Akhir : '))
+
+while awal <= akhir:
+	try:
+		f = open(fileName+'.txt', 'a')
+		print(''.join(bing_image_urls(imgs[awal], limit=1)), file=f)
+		awal += 1
+	except Exception as e:
+		print(e)
+		#print('https://1.bp.blogspot.com/-Q9ZaBpyBN5k/YI6JfF39ylI/AAAAAAAABj0/AOU6aGdsfAMkmecCKfvrnE1VIv7K11fMgCLcBGAsYHQ/s320/allrecipes.png', file=f)
