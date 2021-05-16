d62b54b5c28907081a544f2596c0777ef72c3aed
dagimal <daffagifariakmal@gmail.com>
2021-05-17 01:13:01 +0700

    update pbar

--- txt_bing_image_url.py
+++ txt_bing_image_url.py
@@ -6,11 +6,13 @@ E-mail : daffagifariakmal@gmail.com
 """
 
 from bing_image_urls import bing_image_urls
+from tqdm import tqdm
 
 imgs_file = 'image_keyword.txt'
 imgs = open(imgs_file, 'r').readlines()
-print(imgs[1])
-print('FILE '+imgs_file+' ADA '+str(len(imgs))+' BARIS')
+number_of_lines = len(imgs)
+print(number_of_lines)
+print('FILE '+imgs_file+' ADA '+str(number_of_lines)+' BARIS')
 print('INDEX AWAL NYA ADALAH 0')
 print('=========================\n')
 # INPUT
@@ -18,11 +20,13 @@ fileName = input('Nama File Output : ')
 awal = int(input('Index Awal : '))
 akhir = int(input('Index Akhir : '))
 
+pbar = tqdm(total = int(akhir))
 while awal <= akhir:
 	try:
 		f = open(fileName+'.txt', 'a')
 		print(''.join(bing_image_urls(imgs[awal], limit=1)), file=f)
 		awal += 1
+		pbar.update(1)
 	except Exception as e:
 		print(e)
 		#print('https://1.bp.blogspot.com/-Q9ZaBpyBN5k/YI6JfF39ylI/AAAAAAAABj0/AOU6aGdsfAMkmecCKfvrnE1VIv7K11fMgCLcBGAsYHQ/s320/allrecipes.png', file=f)
